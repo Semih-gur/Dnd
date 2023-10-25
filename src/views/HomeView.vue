77cb3ef1840d4ceec41e0756b7bf7887876f9a83
Abdullah Semih Gür <c2028015@student.cankaya.edu.tr>
2023-10-25 17:07:03 +0300

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="text-center">Dragonborne Tavern</h1>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="4">
+        <v-card class="text-center">Classes</v-card>
+      </v-col>
+      <v-col cols="4"> <v-card class="text-center">Races</v-card> </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="4"> <v-card class="text-center">Spells</v-card> </v-col>
+      <v-col cols="4"> <v-card class="text-center">Feats</v-card> </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="8"> <v-card class="text-center">Coming Soon</v-card></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
