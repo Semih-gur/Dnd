a447e5e4e2ad687af4f5b97452e24f2f2fe0a341
Abdullah Semih Gür <c2028015@student.cankaya.edu.tr>
2023-10-30 14:24:19 +0300

    Nazmi

--- src/views/RacesView.vue
+++ src/views/RacesView.vue
@@ -2,11 +2,11 @@
   <div class="center">
     <v-row  justify="center" row-height="5">
       <v-col v-for="(race,index) in races"  :key="race.id" cols="3">
-        <v-card   height="300" width="300" class="text-center">
+        <v-card @click="goTo(race.label)" height="300" width="300" class="text-center">
           <v-card-title @mouseover="mouseOver(index)" @mouseout="mouseOver(index)" > {{ race.label }}</v-card-title>
           <v-img @mouseover="mouseOver(index)" @mouseout="mouseOver(index)"  src="../assets/image.png"></v-img>
           <v-expand-transition>
-            <v-card class="v-card--reveal" v-if="race.active">This is a secret</v-card>
+            <v-card class="v-card--reveal" v-if="race.active">Nisi eu exercitation aliqua officia eu ut aliquip non reprehenderit mollit. Cillum enim magna esse in ullamco excepteur Lorem. Sint non sunt et ullamco veniam incididunt. Voluptate ipsum mollit laboris aute officia ea officia sint. Occaecat laborum laboris duis labore culpa eu consectetur enim enim veniam sunt deserunt. Culpa commodo ut voluptate magna.</v-card>
           </v-expand-transition>
         </v-card>
       </v-col>
@@ -16,11 +16,17 @@
 
 
 <script>
+import router from "@/router";
 
 export default {
+  
   methods:{
     mouseOver(index){
       this.races[index].active = !this.races[index].active;
+    },
+
+    goTo(label){
+      router.push("/wiki/races/"+label.toLowerCase())
     }
   },
 
