81537164dbceba102dd8ea1dbeca465ddeaea4e0
Abdullah Semih Gür <c2028015@student.cankaya.edu.tr>
2023-11-01 06:33:40 +0300

    A

--- src/views/Class/BarbarianView.vue
+++ src/views/Class/BarbarianView.vue
@@ -35,35 +35,42 @@
         </v-table>
     </div>
     <div class="mt-5">
-        <v-expansion-panels class="center">
-            <v-expansion-panel
-            class="mt-2"
-            title="Level 1"
-            text="Occaecat cupidatat cupidatat consequat ut culpa ex veniam commodo sint tempor incididunt nisi. Nulla quis ad culpa ipsum occaecat officia sit non sunt laborum dolor sunt voluptate elit. Laborum velit do in Lorem laboris. Cillum sunt Lorem proident velit tempor velit do elit. Cupidatat qui irure officia minim enim ut in.
+        <div class="center">
+            <h3>Hit Points</h3>
 
-Exercitation labore laboris incididunt officia nulla non fugiat eiusmod ipsum adipisicing aliquip tempor. Sint est qui exercitation qui in labore nostrud veniam adipisicing dolor reprehenderit. Laborum id consequat sint velit non non velit est ad cupidatat aliquip adipisicing. Eu commodo ea exercitation irure dolore esse Lorem aliquip. Tempor et officia tempor exercitation fugiat. Deserunt proident excepteur ex do. Quis exercitation ea nostrud aute consectetur ad aute laboris cillum.
+            <p class="mt-2">
+                <strong>Hit Dice:</strong> 1d12.
+                <br>
+                <strong>Health:</strong> 12 + Constitution Modifier (+1d12 + Constitution Modifier for every level after).
 
-Dolore in nostrud aliquip excepteur aliqua. Mollit adipisicing tempor elit occaecat minim ullamco consectetur. Ad irure eiusmod adipisicing in nostrud veniam incididunt amet Lorem cupidatat.
+            </p>
+            <br>
+            <h3>Proficiencies</h3>
 
-Fugiat aute nulla veniam non est mollit ex. In magna enim voluptate dolore aute sit sit irure tempor ex. Voluptate magna dolor cillum consequat dolore eiusmod cupidatat aliqua elit. Id culpa laboris velit veniam culpa irure velit officia ullamco. Consectetur ipsum laboris reprehenderit est ullamco aliqua Lorem."
-            >
+            <p>
+                <strong >Armor:</strong> Light Armor, Medium Armor, Shields.
+                <br>
+                <strong>Weapons:</strong> Simple Weapons, Martial Weapons.
+                <br>
+                <strong>Tools:</strong> None.
+                <br>
+                <strong>Saving Throws:</strong> Strenght, Constitution
+                <br>
+                <strong>Skills(Choose 2):</strong> Animal Handling, Athletics, Intimidation, Nature, Perception, and Survival
+            </p>
+            <br>
+            <h3>Starting Equipment</h3>
+            <p>
+                <strong>(a)-</strong>A greataxe <strong>(b)-</strong>Any martial melee weapon
+                <br>
+                <strong>(a)-</strong>two handaxes <strong>(b)-</strong> Any simple weapon
+                <br>
+                An explorer's pack and four javelins
+            </p>
+        </div>
+        <div class="center">
             
-            </v-expansion-panel>
-
-            <v-expansion-panel
-            class="mt-2"
-            title="Level 2"
-            text="Occaecat cupidatat cupidatat consequat ut culpa ex veniam commodo sint tempor incididunt nisi. Nulla quis ad culpa ipsum occaecat officia sit non sunt laborum dolor sunt voluptate elit. Laborum velit do in Lorem laboris. Cillum sunt Lorem proident velit tempor velit do elit. Cupidatat qui irure officia minim enim ut in.
-
-Exercitation labore laboris incididunt officia nulla non fugiat eiusmod ipsum adipisicing aliquip tempor. Sint est qui exercitation qui in labore nostrud veniam adipisicing dolor reprehenderit. Laborum id consequat sint velit non non velit est ad cupidatat aliquip adipisicing. Eu commodo ea exercitation irure dolore esse Lorem aliquip. Tempor et officia tempor exercitation fugiat. Deserunt proident excepteur ex do. Quis exercitation ea nostrud aute consectetur ad aute laboris cillum.
-
-Dolore in nostrud aliquip excepteur aliqua. Mollit adipisicing tempor elit occaecat minim ullamco consectetur. Ad irure eiusmod adipisicing in nostrud veniam incididunt amet Lorem cupidatat.
-
-Fugiat aute nulla veniam non est mollit ex. In magna enim voluptate dolore aute sit sit irure tempor ex. Voluptate magna dolor cillum consequat dolore eiusmod cupidatat aliqua elit. Id culpa laboris velit veniam culpa irure velit officia ullamco. Consectetur ipsum laboris reprehenderit est ullamco aliqua Lorem."
-            >
-            
-            </v-expansion-panel>
-        </v-expansion-panels>
+        </div>
     </div>
 
 
@@ -76,27 +83,27 @@ export default{
     data(){
         return{
             levels:[
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
-                {id:"0",level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
+                {level:"1", profBonus:"+2",feature:"Rage, Unarmored Defense",rages:"2",rageDamage:"+2"},
+                {level:"2", profBonus:"+2",feature:"Reckless Attack, Danger Sense",rages:"2",rageDamage:"+2"},
+                {level:"3", profBonus:"+2",feature:"Primal Path, Primal Knowledge (Optional)",rages:"3",rageDamage:"+2"},
+                {level:"4", profBonus:"+2",feature:"Ability Score Improvement",rages:"3",rageDamage:"+2"},
+                {level:"5", profBonus:"+2",feature:"Extra Attack, Fast Movement",rages:"3",rageDamage:"+2"},
+                {level:"6", profBonus:"+2",feature:"Path feature",rages:"4",rageDamage:"+2"},
+                {level:"7", profBonus:"+2",feature:"Feral Instinct, Instinctive Pounce (Optional)",rages:"4",rageDamage:"+2"},
+                {level:"8", profBonus:"+2",feature:"Ability Score Improvement",rages:"4",rageDamage:"+2"},
+                {level:"9", profBonus:"+2",feature:"Brutal Critical (1 die)",rages:"4",rageDamage:"+3"},
+                {level:"10", profBonus:"+2",feature:"Path feature, Primal Knowledge (Optional)",rages:"4",rageDamage:"+3"},
+                {level:"11", profBonus:"+2",feature:"Relentless Rage",rages:"4",rageDamage:"+3"},
+                {level:"12", profBonus:"+2",feature:"Ability Score Improvement",rages:"5",rageDamage:"+3"},
+                {level:"13", profBonus:"+2",feature:"Brutal Critical (2 dice)",rages:"5",rageDamage:"+3"},
+                {level:"14", profBonus:"+2",feature:"Path feature",rages:"5",rageDamage:"+3"},
+                {level:"15", profBonus:"+2",feature:"Persistent Rage",rages:"5",rageDamage:"+3"},
+                {level:"16", profBonus:"+2",feature:"Ability Score Improvement",rages:"5",rageDamage:"+4"},
+                {level:"17", profBonus:"+2",feature:"Brutal Critical (3 dice)",rages:"6",rageDamage:"+4"},
+                {level:"18", profBonus:"+2",feature:"Indomitable Might",rages:"6",rageDamage:"+4"},
+                {level:"19", profBonus:"+2",feature:"Ability Score Improvement",rages:"6",rageDamage:"+4"},
+                {level:"20", profBonus:"+2",feature:"Primal Champion",rages:"Unlimited",rageDamage:"+4"},
+               
 
                 
             ]
