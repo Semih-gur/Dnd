22e4fdd44e61fed4973b20a3d90d307923a02741
Abdullah Semih Gür <c2028015@student.cankaya.edu.tr>
2023-11-03 10:07:52 +0300

    Rewritten

--- src/views/ClassesView.vue
+++ src/views/ClassesView.vue
@@ -1,8 +1,141 @@
 <template>
-  <div></div>
+  <div class="center">
+    <v-row justify="center" row-height="5">
+      <v-col
+        class=""
+        v-for="(cclass, index) in classes"
+        :key="cclass.id"
+        lg="3"
+        md="4"
+      >
+        <v-card
+          @click="goTo(cclass.label)"
+          height="300"
+          width="300"
+          class="text-center"
+        >
+          <v-card-title
+            @mouseover="mouseOver(index)"
+            @mouseout="mouseOver(index)"
+          >
+            {{ cclass.label }}</v-card-title
+          >
+          <v-img
+            @mouseover="mouseOver(index)"
+            @mouseout="mouseOver(index)"
+            src="../assets/image.png"
+          ></v-img>
+          <v-expand-transition>
+            <v-card class="v-card--reveal" v-if="cclass.active">
+              <v-card-text>{{ cclass.desc }}</v-card-text>
+              <v-card-text>{{ cclass.bonus }}</v-card-text>
+            </v-card>
+          </v-expand-transition>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-export default {};
+import router from "@/router";
+
+export default {
+  methods: {
+    mouseOver(index) {
+      this.classes[index].active = !this.classes[index].active;
+    },
+
+    goTo(label) {
+      router.push("/wiki/classes/" + label.replace(" ", "_").toLowerCase());
+    },
+  },
+
+  data: () => ({
+    reveal: false,
+    classes: [
+      {
+        id: 0,
+        label: "Barbarian",
+        desc: "Dolor excepteur tempor veniam dolore do do aute irure cillum tempor",
+      },
+      {
+        id: 1,
+        label: "Bard",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+      },
+      {
+        id: 2,
+        label: "Cleric",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+      {
+        id: 3,
+        label: "Druid",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+      {
+        id: 4,
+        label: "Fighter",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+      {
+        id: 5,
+        label: "Monk",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+      {
+        id: 6,
+        label: "Paladin",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+      {
+        id: 7,
+        label: "Ranger",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+      {
+        id: 8,
+        label: "Rogue",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+      {
+        id: 9,
+        label: "Sorcerer",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+      {
+        id: 10,
+        label: "Warlock",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+      {
+        id: 11,
+        label: "Wizard",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+    ],
+  }),
+};
 </script>
-p
+
+<style>
+.v-card--reveal {
+  pointer-events: none;
+  bottom: 0;
+  opacity: 1 !important;
+  position: absolute;
+  width: 100%;
+  height: 50%;
+}
+</style>
--- src/views/RacesView.vue
+++ src/views/RacesView.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="center">
-    <v-row  justify="center" row-height="5">
-      <v-col class="mr-n5" v-for="(race,index) in races"  :key="race.id" lg="3" md="4">
-        <v-card @click="goTo(race.label)" height="300" width="300" class="text-center">
-          <v-card-title @mouseover="mouseOver(index)" @mouseout="mouseOver(index)" > {{ race.label }}</v-card-title>
-          <v-img @mouseover="mouseOver(index)" @mouseout="mouseOver(index)"  src="../assets/image.png"></v-img>
+    <v-row justify="center" row-height="5">
+      <v-col
+        class="mr-n5"
+        v-for="(race, index) in races"
+        :key="race.id"
+        lg="3"
+        md="4"
+      >
+        <v-card
+          @click="goTo(race.label)"
+          height="300"
+          width="300"
+          class="text-center"
+        >
+          <v-card-title
+            @mouseover="mouseOver(index)"
+            @mouseout="mouseOver(index)"
+          >
+            {{ race.label }}</v-card-title
+          >
+          <v-img
+            @mouseover="mouseOver(index)"
+            @mouseout="mouseOver(index)"
+            src="../assets/image.png"
+          ></v-img>
           <v-expand-transition>
             <v-card class="v-card--reveal" v-if="race.active">
-              <v-card-text>{{ race.desc}}</v-card-text>
+              <v-card-text>{{ race.desc }}</v-card-text>
               <v-card-text>{{ race.bonus }}</v-card-text>
             </v-card>
           </v-expand-transition>
@@ -17,47 +37,78 @@
   </div>
 </template>
 
-
 <script>
 import router from "@/router";
 
 export default {
-  
-  methods:{
-    mouseOver(index){
+  methods: {
+    mouseOver(index) {
       this.races[index].active = !this.races[index].active;
     },
 
-    goTo(label){
-      router.push("/wiki/races/"+label.toLowerCase())
-    }
+    goTo(label) {
+      router.push("/wiki/races/" + label.replace(" ", "_").toLowerCase());
+    },
   },
 
-    data: () => ({
-      reveal: false,
-      races:[
-      {id:0,label:"Dragonborn",desc:"Dolor excepteur tempor veniam dolore do do aute irure cillum tempor",bonus:"+2 Str"},
-      {id:1,label:"Dwarf",desc:"Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",bonus:"+2 Str"},
-      {id:2,label:"Elf",desc:"Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",bonus:"+2 Str"},
-      {id:3,label:"Gnome",desc:"Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",bonus:"+2 Str"},
-      {id:4,label:"Half-Elf",desc:"Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",bonus:"+2 Str"},
-      {id:5,label:"Halfling",desc:"Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",bonus:"+2 Str"},
-      {id:6,label:"Half-Orc",desc:"Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",bonus:"+2 Str"},
-      {id:7,label:"Tiefling",desc:"Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",bonus:"+2 Str"},
-      ]
-
-    }),
-  }
-
-
-
+  data: () => ({
+    reveal: false,
+    races: [
+      {
+        id: 0,
+        label: "Dragonborn",
+        desc: "Dolor excepteur tempor veniam dolore do do aute irure cillum tempor",
+        bonus: "+2 Str",
+      },
+      {
+        id: 1,
+        label: "Dwarf",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+      {
+        id: 2,
+        label: "Elf",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+      {
+        id: 3,
+        label: "Gnome",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+      {
+        id: 4,
+        label: "Half-Elf",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+      {
+        id: 5,
+        label: "Halfling",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+      {
+        id: 6,
+        label: "Half-Orc",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+      {
+        id: 7,
+        label: "Tiefling",
+        desc: "Cupidatat proident ut Lorem mollit incididunt consectetur cupidatat",
+        bonus: "+2 Str",
+      },
+    ],
+  }),
+};
 </script>
 
 <style>
-
-
 .v-card--reveal {
-  
   pointer-events: none;
   bottom: 0;
   opacity: 1 !important;
