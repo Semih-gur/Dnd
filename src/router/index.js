fe9e0138ac5ae4cac87333cae082757a4cf0416c
Abdullah Semih Gür <c2028015@student.cankaya.edu.tr>
2023-11-13 20:26:31 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -32,49 +32,10 @@ const routes = [
     name: "Barbarian",
     component: BarbarianView,
   },
+  
   {
-    path: "/wiki/classes/barbarian/ancestral_guardian",
-    name: "Ancestral Guardian",
-    component: SubclassView,
-  },
-  {
-    path: "/wiki/classes/barbarian/battlerager",
-    name: "Battlerager",
-    component: SubclassView,
-  },
-  {
-    path: "/wiki/classes/barbarian/beast",
-    name: "Beast",
-    component: SubclassView,
-  },
-  {
-    path: "/wiki/classes/barbarian/berserker",
-    name: "Berserker",
-    component: SubclassView,
-  },
-  {
-    path: "/wiki/classes/barbarian/giant",
-    name: "Giant",
-    component: SubclassView,
-  },
-  {
-    path: "/wiki/classes/barbarian/storm_herald",
-    name: "Storm",
-    component: SubclassView,
-  },
-  {
-    path: "/wiki/classes/barbarian/totem_warrior",
-    name: "Totem",
-    component: SubclassView,
-  },
-  {
-    path: "/wiki/classes/barbarian/wild_magic",
-    name: "Wild",
-    component: SubclassView,
-  },
-  {
-    path: "/wiki/classes/barbarian/zealot",
-    name: "Zealot",
+    path: "/wiki/classes/barbarian/:subclass",
+    name: "Subclass",
     component: SubclassView,
   },
 ];
