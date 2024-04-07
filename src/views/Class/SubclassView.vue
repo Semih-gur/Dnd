6d652c1ab81c8748c6f3ae7dfcd621ee95b58592
Abdullah Semih Gür <c2028015@student.cankaya.edu.tr>
2024-04-07 04:52:29 +0300

    Bard Done

--- src/views/Class/SubclassView.vue
+++ src/views/Class/SubclassView.vue
@@ -30,21 +30,53 @@ export default {
 
       class: [
         {
-          name: "lore",
+          name: "whispers",
           sub: [
-            { lvl: "", title: "", desc: "" },
-            { lvl: "", title: "", desc: "" },
-            { lvl: "", title: "", desc: "" },
-            { lvl: "", title: "", desc: "" },
+            {
+              lvl: "Level 3",
+              title: "Psychic Blades",
+              desc: "When you join the College of Whispers at 3rd level, you gain the ability to make your weapon attacks magically toxic to a creature's mind.\n\nWhen you hit a creature with a weapon attack, you can expend one use of your Bardic Inspiration to deal an additional 2d6 psychic damage to that target. You can do so only once per round on your turn.\n\nThe psychic damage increases when you reach certain levels in this class, increasing to 3d6 at 5th level, 5d6 at 10th level, and 8d6 at 15th level.",
+            },
+            {
+              lvl: "",
+              title: "Words of Terror",
+              desc: "At 3rd level, you learn to infuse innocent-seeming words with an insidious magic that can inspire terror.\n\nIf you speak to a humanoid alone for at least 1 minute, you can attempt to seed paranoia and fear into its mind. At the end of the conversation, the target must succeed on a Wisdom saving throw against your spell save DC or be frightened of you or another creature of your choice. The target is frightened in this way for 1 hour, until it is attacked or damaged, or until it witnesses its allies being attacked or damaged.\n\nIf the target succeeds on its saving throw, the target has no hint that you tried to frighten it.\n\nOnce you use this feature, you can't use it again until you finish a short rest or long rest.",
+            },
+            {
+              lvl: "Level 6",
+              title: "Mantle of Whispers",
+              desc: "At 6th level, you gain the ability to adopt a humanoid's persona. When a humanoid dies within 30 feet of you, you can magically capture its shadow using your reaction. You retain this shadow until you use it or you finish a long rest.\n\nYou can use the shadow as an action. When you do so, it vanishes, magically transforming into a disguise that appears on you. You now look like the dead person, but healthy and alive. This disguise lasts for 1 hour or until you end it as a bonus action.\n\nWhile you're in the disguise, you gain access to all information that the humanoid would freely share with a casual acquaintance. Such information includes general details on its background and personal life, but doesn't include secrets. The information is enough that you can pass yourself off as the person by drawing on its memories.\n\nAnother creature can see through this disguise by succeeding on a Wisdom (Insight) check contested by your Charisma (Deception) check. You gain a +5 bonus to your check.\n\nOnce you capture a shadow with this feature, you can't capture another one with it until you finish a short or long rest.",
+            },
+            {
+              lvl: "Level 14",
+              title: "Shadow Lore",
+              desc: "At 14th level, you gain the ability to weave dark magic into your words and tap into a creature’s deepest fears.\n\nAt 14th level, you gain the ability to weave dark magic into your words and tap into a creature’s deepest fears.\n\nAs an action, you magically whisper a phrase that only one creature of your choice within 30 feet of you can hear. The target must make a Wisdom saving throw against your spell save DC. It automatically succeeds if it doesn’t share a language with you or if it can’t hear you. On a successful saving throw, your whisper sounds like unintelligible mumbling and has no effect.\n\nIf the target fails its saving throw, it is charmed by you for the next 8 hours or until you or your allies attack or damage it. It interprets the whispers as a description of its most mortifying secret.\n\nWhile you gain no knowledge of this secret, the target is convinced you know it. While charmed in this way, the creature obeys your commands for fear that you will reveal its secret. It won’t risk its life for you or fight for you, unless it was already inclined to do so. It grants you favors and gifts it would offer to a close friend.\n\nWhen the effect ends, the creature has no understanding of why it held you in such fear.\n\nOnce you use this feature, you can’t use it again until you finish a long rest.",
+            },
           ],
         },
         {
-          name: "lore",
+          name: "valor",
           sub: [
-            { lvl: "", title: "", desc: "" },
-            { lvl: "", title: "", desc: "" },
-            { lvl: "", title: "", desc: "" },
-            { lvl: "", title: "", desc: "" },
+            {
+              lvl: "Level 3",
+              title: "Bonus Proficiencies",
+              desc: "When you join the College of Valor at 3rd level, you gain proficiency with medium armor, shields, and martial weapons.",
+            },
+            {
+              lvl: "",
+              title: "Combat Inspiration",
+              desc: "Also at 3rd level, you learn to inspire others in battle. A creature that has a Bardic Inspiration die from you can roll that die and add the number rolled to a weapon damage roll it just made. Alternatively, when an attack roll is made against the creature, it can use its reaction to roll the Bardic Inspiration die and add the number rolled to its AC against that attack, after seeing the roll but before knowing whether it hits or misses.",
+            },
+            {
+              lvl: "Level 6",
+              title: "Extra Attack",
+              desc: "Starting at 6th level, you can attack twice, instead of once, whenever you take the Attack action on your turn.",
+            },
+            {
+              lvl: "Level 14",
+              title: "Battle Magic",
+              desc: "At 14th level, you have mastered the art of weaving spellcasting and weapon use into a single harmonious act. When you use your action to cast a bard spell, you can make one weapon attack as a bonus action.",
+            },
           ],
         },
         {
@@ -58,10 +90,23 @@ export default {
             {
               lvl: "",
               title: "Fighting Style",
-              desc: "At 3rd level, you adopt a particular style of fighting as your specialty. Choose one of the following options. You can't take a Fighting Style option more than once, even if you later get to choose again.\n<ul><li><strong>Dueling:</strong></li></ul>",
+              desc: "At 3rd level, you adopt a particular style of fighting as your specialty. Choose one of the following options. You can't take a Fighting Style option more than once, even if you later get to choose again.\n\n<ul><li><strong>Dueling: </strong> When you are wielding a melee weapon in one hand and no other weapons, you gain a +2 bonus to damage rolls with that weapon.</li>\n<li><strong>Two-Weapon Fighting: </strong>When you engage in two-weapon fighting, you can add your ability modifier to the damage of the second attack.</li></ul>",
             },
-            { lvl: "", title: "", desc: "" },
-            { lvl: "", title: "", desc: "" },
+            {
+              lvl: "",
+              title: "Blade Flourish",
+              desc: "Whenever you take the Attack action on your turn, your walking speed increases by 10 feet until the end of the turn, and if a weapon attack that you make as part of this action hits a creature, you can use one of the following Blade Flourish options of your choice. You can use only one Blade Flourish option per turn.\n\n<ul><li><strong>Defensive Flourish.</strong> </li>You can expend one use of your Bardic Inspiration to cause the weapon to deal extra damage to the target you hit. The damage equals the number you roll on the Bardic Inspiration die. You also add the number rolled to your AC until the start of your next turn.\n\n<li><strong>Slashing Flourish.</strong>You can expend one use of your Bardic Inspiration to cause the weapon to deal extra damage to the target you hit and to any other creature of your choice that you can see within 5 feet of you. The damage equals the number you roll on the Bardic Inspiration die.</li>\n\n<li><strong>Mobile Flourish. </strong>You can expend one use of your Bardic Inspiration to cause the weapon to deal extra damage to the target you hit. The damage equals the number you roll on the Bardic Inspiration die. You can also push the target up to 5 feet away from you, plus a number of feet equal to the number you roll on that die. You can then immediately use your reaction to move up to your walking speed to an unoccupied space within 5 feet of the target.</li></ul>",
+            },
+            {
+              lvl: "Level 6",
+              title: "Extra Attack",
+              desc: "Starting at 6th level, you can attack twice, instead of once, whenever you take the Attack action on your turn",
+            },
+            {
+              lvl: "",
+              title: "Master's Flourish",
+              desc: "Starting at 14th level, whenever you use a Blade Flourish option, you can roll a d6 and use it instead of expending a Bardic Inspiration die.",
+            },
           ],
         },
         {
