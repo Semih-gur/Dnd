4adc4bb86b9f0d426636b49b69c4ecedb1e1d627
Abdullah Semih Gür <c2028015@student.cankaya.edu.tr>
2024-05-20 12:53:15 +0300

    Barb Wild magic done

--- src/views/Class/SubclassView.vue
+++ src/views/Class/SubclassView.vue
@@ -424,11 +424,75 @@ export default {
         {
           name: "wild_magic",
           sub: [
-            { lvl: "", title: "", desc: "" },
-            { lvl: "", title: "", desc: "" },
-            { lvl: "", title: "", desc: "" },
-            { lvl: "", title: "", desc: "" },
-            { lvl: "", title: "", desc: "" },
+            {
+              lvl: "Level 3",
+              title: "Magic Awareness",
+              desc: "When you choose this path at 3rd level, as an action, you can open your awareness to the presence of concentrated magic. Until the end of your next turn, you know the location of any spell or magic item within 60 feet of you that isn’t behind total cover. When you sense a spell, you learn which school of magic it belongs to.\n\nYou can use this feature a number of times equal to your proficiency bonus, and you regain all expended uses when you finish a long rest.",
+            },
+            {
+              lvl: "",
+              title: "Wild Surge",
+              desc: `Also at 3rd level, the magical energy roiling inside you sometimes erupts from you. When you enter your rage, roll on the Wild Magic table to determine the magical effect produced.\n\nIf the effect requires a saving throw, the DC equals 8 + your proficiency bonus + your Constitution modifier.
+          
+          <table>
+            <thead>
+              <tr>
+                <th>d8</th>
+                <th>Effect</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Roll twice on this table, ignoring further rolls of 1.</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>You cast Fireball centered on yourself.</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>For the next minute, you can teleport up to 20 feet as a bonus action on each of your turns.</td>
+              </tr>
+              <tr>
+                <td>4</td>
+                <td>You cast Polymorph on yourself. If you fail the saving throw, you turn into a sheep for the spell's duration.</td>
+              </tr>
+              <tr>
+                <td>5</td>
+                <td>A spectral shield hovers near you for the next minute, granting you a +2 bonus to AC and immunity to Magic Missile.</td>
+              </tr>
+              <tr>
+                <td>6</td>
+                <td>For the next minute, you are surrounded by faint, ethereal music.</td>
+              </tr>
+              <tr>
+                <td>7</td>
+                <td>You cast Fly on a random creature within 60 feet of you.</td>
+              </tr>
+              <tr>
+                <td>8</td>
+                <td>You regain 2d10 hit points.</td>
+              </tr>
+            </tbody>
+          </table>
+        `,
+            },
+            {
+              lvl: "Level 6",
+              title: "Bolstering Magic",
+              desc: "Beginning at 6th level, you can harness your wild magic to bolster yourself or a companion. As an action, you can touch one creature (which can be yourself) and confer one of the following benefits of your choice to that creature:\n\nFor 10 minutes, the creature can roll a d3 whenever making an attack roll or an ability check and add the number rolled to the d20 roll.\n\nRoll a d3. The creature regains one expended spell slot, the level of which equals the number rolled or lower (the creature’s choice). Once a creature receives this benefit, that creature can’t receive it again until after a long rest.\n\nYou can take this action a number of times equal to your proficiency bonus, and you regain all expended uses when you finish a long rest.",
+            },
+            {
+              lvl: "Level 10",
+              title: "Unstable Backlash",
+              desc: "At 10th level, when you are imperiled during your rage, the magic within you can lash out; immediately after you take damage or fail a saving throw while raging, you can use your reaction to roll on the Wild Magic table and immediately produce the effect rolled. This effect replaces your current Wild Magic effect.",
+            },
+            {
+              lvl: "Level 14",
+              title: "Controlled Surge",
+              desc: "At 14th level, whenever you roll on the Wild Magic table, you can roll the die twice and choose which of the two effects to unleash. If you roll the same number on both dice, you can ignore the number and choose any effect on the table.",
+            },
           ],
         },
       ],
