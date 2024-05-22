217b1f90b1f1e9da50d283b466dd32ceedb38c7f
Abdullah Semih Gür <c2028015@student.cankaya.edu.tr>
2024-05-22 22:32:35 +0300

    Added imgs to wiki home

--- src/views/WikiHomeView.vue
+++ src/views/WikiHomeView.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class="center">
+  <div class="center mt-16">
     <v-row justify="center" row-height="5">
       <v-col cols="3">
         <v-card @click="classes()" class="text-center">
           <v-card-title>Classes</v-card-title>
-          <v-img src="../assets/image.png"></v-img>
+          <v-img src="../assets/wikiPage/class.png"></v-img>
         </v-card>
       </v-col>
       <v-col cols="3">
         <v-card class="text-center"
           ><v-card @click="races()" class="text-center">
             <v-card-title>Races</v-card-title>
-            <v-img src="../assets/image.png"></v-img> </v-card
+            <v-img src="../assets/wikiPage/spells.png"></v-img> </v-card
         ></v-card>
       </v-col>
       <v-col cols="3">
         <v-card class="text-center"
           ><v-card @click="spells()" class="text-center">
             <v-card-title>Spells</v-card-title>
-            <v-img src="../assets/image.png"></v-img> </v-card
+            <v-img src="../assets/wikiPage/spells.png"></v-img> </v-card
         ></v-card>
       </v-col>
     </v-row>
@@ -27,21 +27,21 @@
         <v-card class="text-center"
           ><v-card @click="bg()" class="text-center">
             <v-card-title>Backgrounds</v-card-title>
-            <v-img src="../assets/image.png"></v-img> </v-card
+            <v-img src="../assets/wikiPage/backgrounds.png"></v-img> </v-card
         ></v-card>
       </v-col>
       <v-col cols="3">
         <v-card class="text-center"
           ><v-card class="text-center">
             <v-card-title>Feats</v-card-title>
-            <v-img src="../assets/image.png"></v-img> </v-card
+            <v-img src="../assets/wikiPage/quill.png"></v-img> </v-card
         ></v-card>
       </v-col>
       <v-col cols="3">
         <v-card class="text-center"
           ><v-card class="text-center">
             <v-card-title>Items</v-card-title>
-            <v-img src="../assets/image.png"></v-img> </v-card
+            <v-img src="../assets/wikiPage/items.png"></v-img> </v-card
         ></v-card>
       </v-col>
     </v-row>
